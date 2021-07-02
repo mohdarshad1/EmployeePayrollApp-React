f0bc17dc24a11f5fd3c8a2154f1ec69d9616bad2
mohdarshad1 <mdarshad181@gmail.com>
2021-07-02 19:08:32 +0530

    [Mohd Arshad] : Payroll-form Page To add employee

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import PayrollForm from './components/payroll-form/payroll-form';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route, Redirect
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <Switch>
+          <Route exact path = "">
+            <PayrollForm />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
